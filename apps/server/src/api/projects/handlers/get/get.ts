7b78c0fbfab5378865dac9aea5058472b4e443b6
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-17 01:32:21 +0200

    Add error handling and response formatting in project get handler

--- apps/server/src/api/projects/handlers/get/get.ts
+++ apps/server/src/api/projects/handlers/get/get.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import Joi from "joi";
+import {
+  FetchRailwayApiError,
+  Projects,
+  ValidationError,
+} from "@monorepo/types";
 import query from "./query";
 
 const apiUrl = process.env.RAILWAY_API_URL;
@@ -10,7 +15,11 @@ const schema = Joi.object({
 const handler = async (req: Request, res: Response) => {
   const { error, value } = schema.validate(req.query);
   if (error) {
-    return res.status(400).json({ error: error.details[0]?.message });
+    const { status, ...errorBody } = new ValidationError(
+      error.details[0]?.message
+    );
+
+    return res.status(400).json(errorBody);
   }
 
   const { token } = value;
@@ -24,11 +33,17 @@ const handler = async (req: Request, res: Response) => {
     body: JSON.stringify({
       query,
     }),
-  }).then((res) => res.json());
+  })
+    .then((res) => res.json() as Promise<Projects>)
+    .catch((error) => {
+      const { status, ...errorBody } = new FetchRailwayApiError(
+        "projects",
+        error.message
+      );
+      return res.status(status).json(errorBody);
+    });
 
-  return res
-    .status(200)
-    .json(projects.data.me.projects.edges.map((edge: any) => edge.node));
+  return res.status(200).json(projects);
 };
 
 export default handler;
