d8bfe75a7fd0f677c620032d88474717f4b9c1c5
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-18 20:37:44 +0200

    Refactor App component and remove unused code

---
+++ apps/ui/src/components/App/App.test.ts
@@ -0,0 +1,60 @@
+import { mockComponent } from "@/test-helpers";
+import App from "./App";
+
+jest.mock("@/components/TokenInput", () => mockComponent("TokenInput"));
+jest.mock("@/components/Instructions", () => mockComponent("Instructions"));
+jest.mock("@/components/Projects", () => mockComponent("Projects"));
+
+const render = () => {
+  const app = new App();
+  document.body.appendChild(app.element);
+};
+
+describe("App", () => {
+  beforeEach(() => {
+    render();
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = "";
+  });
+
+  it("should render", () => {
+    expect(document.body).toMatchSnapshot();
+  });
+
+  describe("header", () => {
+    it("should render the correct title", () => {
+      const header = document.querySelector("header");
+
+      expect(header).not.toBeNull();
+      expect(header!.textContent).toContain("Railway Playground");
+    });
+
+    it("should render the correct title link", () => {
+      const titleLink = document.querySelector("a");
+
+      expect(titleLink!.getAttribute("href")).toBe("/");
+    });
+  });
+
+  it("should render the TokenInput component", () => {
+    const tokenInput = document.querySelector("[data-component='TokenInput']");
+
+    expect(tokenInput).not.toBeNull();
+  });
+
+  it("should render the Instructions component", () => {
+    const instructions = document.querySelector(
+      "[data-component='Instructions']"
+    );
+
+    expect(instructions).not.toBeNull();
+  });
+
+  it("should render the Projects component", () => {
+    const projects = document.querySelector("[data-component='Projects']");
+
+    expect(projects).not.toBeNull();
+  });
+});
---
+++ apps/ui/src/components/App/App.ts
@@ -0,0 +1,59 @@
+import { Component } from "@/types";
+import TokenInput from "@/components/TokenInput";
+import Instructions from "@/components/Instructions";
+import Projects from "@/components/Projects";
+
+import styles from "./App.module.css";
+
+class App extends Component<DocumentFragment> {
+  constructor() {
+    super();
+
+    this._element = document.createDocumentFragment();
+
+    this.render();
+  }
+
+  override render() {
+    if (!this._element) {
+      return;
+    }
+
+    this._element.textContent = "";
+
+    const header = document.createElement("header");
+    header.className = styles.header!;
+    const headerInner = document.createElement("div");
+    headerInner.className = "container";
+    headerInner.innerHTML = `
+      <h1 class="title" data-testid="title">
+        <a href="/" title="Railway Playground Home" data-testid="title-link">
+          Railway Playground
+        </a>
+      </h1>
+    `;
+    header.appendChild(headerInner);
+    this._element.appendChild(header);
+
+    const wrapper = document.createElement("main");
+    wrapper.className = "container";
+
+    const input = new TokenInput();
+    const inputSection = document.createElement("section");
+
+    const instructions = new Instructions();
+    inputSection.appendChild(input.element);
+    inputSection.appendChild(instructions.element);
+
+    wrapper.appendChild(inputSection);
+
+    const projects = new Projects();
+    const projectsSection = document.createElement("section");
+    projectsSection.appendChild(projects.element);
+    wrapper.appendChild(projectsSection);
+
+    this._element.appendChild(wrapper);
+  }
+}
+
+export default App;
