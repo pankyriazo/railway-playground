5c027d4a89449e755f9379cdf6e67678e19f1607
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-19 00:50:21 +0200

    Update Dockerfile and environment configuration

--- apps/ui/src/environment.ts
+++ apps/ui/src/environment.ts
@@ -1,6 +1,6 @@
 export const getServerUrl = () => {
   if (__IS_PRODUCTION__) {
-    return "http://railway-playground.up.railway.app";
+    return "http://railway-playground.railway.internal";
   }
 
   return "http://localhost:3001";
