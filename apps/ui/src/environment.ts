ed2ad49dea72ed392d65d17597ccaf45322ecb77
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-18 23:44:27 +0200

    Update server URL in environment.ts

--- apps/ui/src/environment.ts
+++ apps/ui/src/environment.ts
@@ -1,6 +1,6 @@
 export const getServerUrl = () => {
   if (__IS_PRODUCTION__) {
-    return "http://localhost:3001";
+    return "http://railway-playground.up.railway.app";
   }
 
   return "http://localhost:3001";
