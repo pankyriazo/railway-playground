b2c6e8adb3c4f668ed1f1a666b8c4c101dc81f18
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-18 21:51:47 +0200

    Add mock data for getProjectList API

---
+++ apps/ui/src/api/__mocks__/getProjectList.ts
@@ -0,0 +1,62 @@
+import { GetProjectListResponse } from "@/api/types";
+
+export const mockGetProjectList: GetProjectListResponse = {
+  data: {
+    me: {
+      projects: {
+        edges: [
+          {
+            node: {
+              id: "a20a0d44-a6de-4804-acf1-58d436570fd7",
+              name: "nauseating-cup",
+              services: {
+                edges: [
+                  {
+                    node: {
+                      id: "0e62e7bf-2008-4452-bd32-e57cec3bb9a2",
+                      name: "fascinated-motion",
+                    },
+                  },
+                  {
+                    node: {
+                      id: "7ab42902-aae8-47b7-9976-81496dc61834",
+                      name: "Postgres",
+                    },
+                  },
+                ],
+              },
+              plugins: {
+                edges: [],
+              },
+              environments: {
+                edges: [
+                  {
+                    node: {
+                      id: "1beb4373-6b1f-43cb-8150-0e7214acea82",
+                      name: "production",
+                    },
+                  },
+                  {
+                    node: {
+                      id: "632da482-c5b1-4570-87af-9ea225c335cb",
+                      name: "staging",
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+};
+
+export const mockGetProjectListError: GetProjectListResponse = {
+  data: null,
+  errors: [
+    {
+      message: "Unauthorized",
+    },
+  ],
+};
