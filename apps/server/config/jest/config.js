69839721eb67fd3fe0602b37b65f8f62db385df4
Panos Kyriazopoulos <pankyriazo@gmail.com>
2024-03-19 16:22:24 +0200

    Add setup file for Jest environment variables

--- apps/server/config/jest/config.js
+++ apps/server/config/jest/config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   rootDir: "../../",
   testMatch: ["<rootDir>/src/**/*.test.ts"],
+  setupFiles: ["<rootDir>/config/jest/setEnvVars.js"],
   setupFilesAfterEnv: ["<rootDir>/config/jest/setup.js"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
---
+++ apps/server/config/jest/setEnvVars.js
@@ -0,0 +1,1 @@
+process.env.RAILWAY_API_URL = "test-railway-api-url";
